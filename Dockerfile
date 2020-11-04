b736b37678b1e0fb68e8b3905ca9ac4054681140
EC2 Default User <ec2-user@ip-172-31-92-164.ec2.internal>
2020-11-04 05:16:36 +0000

    add web

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@ FROM golang:1.15.3-alpine
 WORKDIR /tick
 ADD . /tick
 RUN cd /tick && go build -o tick
+EXPOSE 9936
 ENTRYPOINT ./tick
--- tick.go
+++ tick.go
@@ -2,11 +2,49 @@ package main
 
 import (
 	"fmt"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 	"math/rand"
+	"net/http"
+	"sync"
 	"time"
 )
 
 var letter = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
+
+type SafeQueue struct {
+	mu   sync.Mutex
+	data []string
+}
+
+func (q *SafeQueue) Enqueue(s string) {
+	q.mu.Lock()
+	q.data = append(q.data, s)
+	q.mu.Unlock()
+}
+
+func (q *SafeQueue) Getstring() string {
+	q.mu.Lock()
+	ret := ""
+
+	start := 0
+	if len(q.data) > 5 {
+		start = len(q.data) - 5
+	}
+
+	for cnt, idx := 0, start; idx < len(q.data) && cnt < 5; idx, cnt = idx+1, cnt+1 {
+		ret += q.data[idx] + "\n"
+	}
+
+	// if len(q.data) == 0 {
+	// 	q.mu.Unlock()
+	// 	return ""
+	// }
+	// v := q.data[0]
+	q.data = q.data[start:]
+	q.mu.Unlock()
+	return ret
+}
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
@@ -24,17 +62,34 @@ func main() {
 
 	ticker := time.NewTicker(5 * time.Second)
 	done := make(chan bool)
+	queue := SafeQueue{data: []string{}}
 
 	randstr := RandomString(20)
 
-	for {
-		select {
-		case <-done:
-			return
-		case t := <-ticker.C:
-			fmt.Println(t, ": ", randstr)
+	go func() {
+		for {
+			select {
+			case <-done:
+				return
+			case t := <-ticker.C:
+				str := t.Format("2006-01-02 15:04:05") + ": " + randstr
+				queue.Enqueue(str)
+			}
 		}
-	}
+	}()
 
-	time.Sleep(16 * time.Second)
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Route => handler
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, queue.Getstring())
+	})
+
+	// Start server
+	fmt.Println("Start Server from port 9936")
+	e.Logger.Fatal(e.Start(":9936"))
 }
