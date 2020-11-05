c2655a96a4885c4bda738bd4460f8c27b98f0401
EC2 Default User <ec2-user@ip-172-31-92-164.ec2.internal>
2020-11-05 02:25:02 +0000

    split app to 2

--- tick.go
+++ tick.go
@@ -1,49 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
 	"math/rand"
-	"net/http"
-	"sync"
 	"time"
+	"io/ioutil"
 )
 
 var letter = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
 
-type SafeQueue struct {
-	mu   sync.Mutex
-	data []string
-}
 
-func (q *SafeQueue) Enqueue(s string) {
-	q.mu.Lock()
-	q.data = append(q.data, s)
-	q.mu.Unlock()
-}
-
-func (q *SafeQueue) Getstring() string {
-	q.mu.Lock()
-	ret := ""
-
-	start := 0
-	if len(q.data) > 5 {
-		start = len(q.data) - 5
-	}
-
-	for cnt, idx := 0, start; idx < len(q.data) && cnt < 5; idx, cnt = idx+1, cnt+1 {
-		ret += q.data[idx] + "\n"
-	}
-
-	// if len(q.data) == 0 {
-	// 	q.mu.Unlock()
-	// 	return ""
-	// }
-	// v := q.data[0]
-	q.data = q.data[start:]
-	q.mu.Unlock()
-	return ret
+	
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
 }
 
 func init() {
@@ -58,38 +28,31 @@ func RandomString(n int) string {
 	return string(b)
 }
 
+func writeshowme(str string){
+	println(str)
+	d1 := []byte(str)
+	println(d1)
+    err := ioutil.WriteFile("/home/ec2-user/environment/showme.txt", d1, 0644)
+    check(err)
+}
+
+
 func main() {
 
 	ticker := time.NewTicker(5 * time.Second)
 	done := make(chan bool)
-	queue := SafeQueue{data: []string{}}
 
 	randstr := RandomString(20)
 
-	go func() {
-		for {
+
+	for {
 			select {
 			case <-done:
 				return
 			case t := <-ticker.C:
-				str := t.Format("2006-01-02 15:04:05") + ": " + randstr
-				queue.Enqueue(str)
+				str := t.Format("2006-01-02 15:04:05") + " : " + randstr
+				writeshowme(str)
 			}
-		}
-	}()
+	}
 
-	e := echo.New()
-
-	// Middleware
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-
-	// Route => handler
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, queue.Getstring())
-	})
-
-	// Start server
-	fmt.Println("Start Server from port 9936")
-	e.Logger.Fatal(e.Start(":9936"))
 }
